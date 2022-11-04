5c8121d18dede2f895052fbf6aac5dc64f091d6e
Yossi <asnafy.junior@gmail.com>
2022-11-04 09:12:37 +0200

    back to start

--- server.ts
+++ server.ts
@@ -1,29 +1,11 @@
 import express from 'express';
 import mongoose from "mongoose";
 require('dotenv').config() //===> insert high as possible, before routs
-const router = express.Router();
-
-
-import { addUser, getUser, updateUser, loadUser, getOutUser, loadAllUsers } from "./controllers/usersCont"
-// import router from './routes/userRoutes';
-    // .get('/get-user', isAdmin, getUser)
-    router.get('/get-user', getUser)
-    router.get('/get-out-user', getOutUser)
-    router.get('/load-all-users', loadAllUsers)
-    router.get('/load-user', loadUser)
-    router.post('/add-user', addUser)
-    router.patch('/update-user', updateUser)
-
-
 
 import userRoutes from "./routes/userRoutes"
 import wordRoutes from "./routes/wordRoutes"
 
-// var express = require('express');
 
-// const routes = require('./routes')
-// const userRoutes = require("./routes/userRoutes")
-// const wordRoutes = require("./routes/wordRoutes")
 
 console.log(process.env.ENV)
 
