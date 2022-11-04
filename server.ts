e8a79a7ad4e2c940a13f2c1c2544e044d632dce5
Yossi <asnafy.junior@gmail.com>
2022-11-04 15:46:21 +0200

    ok

--- server.ts
+++ server.ts
@@ -11,11 +11,11 @@ console.log(process.env.ENV)
 
 const cookieParser = require('cookie-parser')
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT || 3005;
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser()); 
 
 app.use('/users', userRoutes)
 app.use('/words', wordRoutes)
