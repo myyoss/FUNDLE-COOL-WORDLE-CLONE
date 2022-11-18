90057a5fee4a51d1c98ba79a42d8e780403071a0
Yossi <asnafy.junior@gmail.com>
2022-11-18 17:16:32 +0200

    // "module": "commonjs",

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.use(express.json()); 
 app.use(cookieParser());
 
-app.use('/users', userRoutes)
+app.use('/users', userRoutes) 
 app.use('/words', wordRoutes)
 
 
