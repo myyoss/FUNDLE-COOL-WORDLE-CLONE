351b4c0ed78164093e8c9c281e9ec3b731e60b1d
Yossi <asnafy.junior@gmail.com>
2022-11-18 14:20:35 +0200

    module.exports = router; out

--- dist/server.js
+++ dist/server.js
@@ -6,23 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 require('dotenv').config(); //===> insert high as possible, before routs
-// import userRoutes from "./routes/userRoutes"
-// import wordRoutes from "./routes/wordRoutes"
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const wordRoutes_1 = __importDefault(require("./routes/wordRoutes"));
 // require('./routes/userRoutes')
 // require('./routes/wordRoutes')
-
 const config = require('./routes/userRoutes');
 console.log(process.env.ENV);
 const cookieParser = require('cookie-parser');
-const userRoutes = require('./routes/userRoutes');
-const wordRoutes = require('./routes/wordRoutes');
+// const userRoutes = require('./routes/userRoutes')
+// const wordRoutes = require('./routes/wordRoutes')
 const app = express_1.default();
 const port = process.env.PORT || 3004;
 app.use(express_1.default.static("public"));
 app.use(express_1.default.json());
 app.use(cookieParser());
-app.use('/users', userRoutes);
-app.use('/words', wordRoutes);
+app.use('/users', userRoutes_1.default);
+app.use('/words', wordRoutes_1.default);
 // mongoose.connect('mongodb+srv://asnafy:ZyTcRnGlhXYqaYjE@cluster0.xgv3d.mongodb.net/fundle?retryWrites=true&w=majority');
 // MONGODB_URI=mongodb+srv://michaeldubovik:michaeldubovik1991@cluster0.y9ozg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const uri = process.env.MONGODB_URI;
