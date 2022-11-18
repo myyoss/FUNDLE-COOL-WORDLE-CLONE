01bd85b26233732ca4d803c62376876d70e784a6
Yossi <asnafy.junior@gmail.com>
2022-11-18 12:25:42 +0200

    update

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
 require('dotenv').config() //===> insert high as possible, before routs
-
 import userRoutes from "./routes/userRoutes"
 import wordRoutes from "./routes/wordRoutes"
 
@@ -11,15 +10,18 @@ console.log(process.env.ENV)
 
 const cookieParser = require('cookie-parser')
 
-const app = express(); 
-const port = process.env.PORT || 3005;
+const app = express();
+const port = process.env.PORT || 3004;
 app.use(express.static("public"));
-app.use(express.json());
-app.use(cookieParser()); 
+app.use(express.json()); 
+app.use(cookieParser());
 
 app.use('/users', userRoutes)
 app.use('/words', wordRoutes)
 
+
+// mongoose.connect('mongodb+srv://asnafy:ZyTcRnGlhXYqaYjE@cluster0.xgv3d.mongodb.net/fundle?retryWrites=true&w=majority');
+// MONGODB_URI=mongodb+srv://michaeldubovik:michaeldubovik1991@cluster0.y9ozg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 const uri:any = process.env.MONGODB_URI;
 mongoose.connect(uri);
