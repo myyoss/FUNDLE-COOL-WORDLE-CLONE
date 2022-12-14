5d60897d21c8905d1987f3573b7e73508733a36a
Yossi <asnafy.junior@gmail.com>
2022-12-14 13:52:45 +0200

    commonjs

--- dist/server.js
+++ dist/server.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 require('dotenv').config(); //===> insert high as possible, before routs
-const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
-const wordRoutes_1 = __importDefault(require("./routes/wordRoutes"));
+// import {userRoutes} from "./routes/userRoutes"
+// import {wordRoutes} from "./routes/wordRoutes"
+const userRoutes = require('./routes/userRoutes');
+const wordRoutes = require('./routes/wordRoutes');
 // console.log(process.env.ENV)
 const cookieParser = require('cookie-parser');
 const app = express_1.default();
@@ -15,8 +17,8 @@ const port = process.env.PORT || 3006;
 app.use(express_1.default.static("public"));
 app.use(express_1.default.json());
 app.use(cookieParser());
-app.use('/users', userRoutes_1.default);
-app.use('/words', wordRoutes_1.default);
+app.use('/users', userRoutes);
+app.use('/words', wordRoutes);
 const uri = process.env.MONGODB_URI;
 mongoose_1.default.connect(uri);
 app.listen(port, () => {
